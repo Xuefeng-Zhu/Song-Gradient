0c0c4473814fafb42145eed8e2a7bd8c74c5a6ef
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-11-12 23:21:08 -0600

    add more data

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,8 +2,8 @@
 
 /* Controllers */
 angular.module('myApp.controllers', [])
-.controller('MainCtrl', ['$scope', '$rootScope', '$location',
-    function($scope, $rootScope, $location) {
+.controller('MainCtrl', ['$scope', '$rootScope', '$sce',
+    function($scope, $rootScope, $sce) {
         $scope.attrs = ['energy', 'liveness', 'speechiness', 'acousticness', 'danceability', 'instrumentalness', 'valence'];
         $scope.red = 'energy';
         $scope.green = 'liveness';
@@ -16,7 +16,31 @@ angular.module('myApp.controllers', [])
             }
             $scope.playlists[$scope.newList] = playlists[$scope.newList];
             $scope.newList = "";
+            console.log($scope.playlists)
         }
+
+        $scope.showDetail = function(song){
+            $scope.selectedSong = song;
+            SC.initialize({
+              client_id: '26311d46f4fb4dee2e2f83110a512097'
+            });
+            // find all sounds of buskers licensed under 'creative commons share alike'
+            SC.get('/tracks', { q: song.song_name}, function(tracks) {
+                var i = 0;
+                while (!tracks[i].streamable && i < 50){
+                    i ++;
+                }
+                $scope.selectedSong['steam'] = tracks[i].stream_url + "?client_id=26311d46f4fb4dee2e2f83110a512097";
+                $scope.$apply();
+            });
+
+            $('.modal').modal('show');
+        }
+
+        $scope.trustSrc = function(src) {
+            return $sce.trustAsResourceUrl(src);
+        }
+
 
     }
 ])
